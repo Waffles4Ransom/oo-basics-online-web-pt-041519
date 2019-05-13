1337da4d9f345166518577e830838be90f14ecec
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-13 00:20:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material,:condition
+  attr_reader :brand
+  
+  def initialize (brand)
+    @brand = brand
+  end
+  
+  def cobble
+    if Shoe.condition == "old"
+      Shoe.condition = "new"
+    end
+    puts "Your shoe is good as new."
+  end
+  
+end
